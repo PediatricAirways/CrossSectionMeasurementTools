293b8add5006cadbc830b47f9fccf592489e5f32
Cory Quammen <cory.quammen@kitware.com>
2015-01-24 12:52:54 -0500

    Got contouring filter from points working

--- TestCrossSectionMeasurement.cxx
+++ TestCrossSectionMeasurement.cxx
@@ -1,7 +1,51 @@
 #include <iostream>
 
+#include "vtkCrossSectionImageFilter.h"
+
+#include <vtkImageThreshold.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkXMLImageDataReader.h>
+#include <vtkXMLPolyDataWriter.h>
+
+
 int main(int argc, char* argv[])
 {
+  if (argc < 3)
+    {
+    std::cerr << "Usage: " << argv[0] << " <image file> <output polydata file>\n";
+    return EXIT_FAILURE;
+    }
+
+  std::string inputImageFile(argv[1]);
+  std::string outputPolyDataFile(argv[2]);
+
+  // Load image file
+  vtkSmartPointer<vtkXMLImageDataReader> reader =
+    vtkSmartPointer<vtkXMLImageDataReader>::New();
+  reader->SetFileName( argv[1] );
+
+  // Set up cross section sample points
+  vtkSmartPointer<vtkPoints> samplePoints = vtkSmartPointer<vtkPoints>::New();
+  samplePoints->SetNumberOfPoints( 2 );
+  samplePoints->SetPoint( 0, 3, 170, -166 );
+  samplePoints->SetPoint( 1, 8.095, 178.0, -172.7 );
+  //samplePoints->SetPoint( 2, 0, 0, 0 );
+
+  vtkSmartPointer<vtkPolyData> pointSet = vtkSmartPointer<vtkPolyData>::New();
+  pointSet->SetPoints( samplePoints );
+
+  vtkSmartPointer<vtkCrossSectionImageFilter> crossSectionFilter =
+    vtkSmartPointer<vtkCrossSectionImageFilter>::New();
+  crossSectionFilter->SetInputConnection( 0, reader->GetOutputPort() );
+  crossSectionFilter->SetInputDataObject( 1, pointSet );
+
+  vtkSmartPointer<vtkXMLPolyDataWriter> polyDataWriter =
+    vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+  polyDataWriter->SetFileName( argv[2] );
+  polyDataWriter->SetInputConnection( crossSectionFilter->GetOutputPort() );
+  polyDataWriter->Update();
 
   return EXIT_SUCCESS;
 }
--- vtkCrossSectionImageFilter.cxx
+++ vtkCrossSectionImageFilter.cxx
@@ -1,16 +1,25 @@
 #include "vtkCrossSectionImageFilter.h"
 
 #include "vtkAlgorithm.h"
+#include "vtkContourFilter.h"
+#include "vtkDataSet.h"
 #include "vtkImageData.h"
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
+#include "vtkPointData.h"
+#include "vtkPointSet.h"
+#include "vtkProbeFilter.h"
+#include "vtkSmartPointer.h"
+#include "vtkThreshold.h"
+
 
 vtkStandardNewMacro(vtkCrossSectionImageFilter);
 
 //----------------------------------------------------------------------------
 vtkCrossSectionImageFilter::vtkCrossSectionImageFilter()
 {
+  this->SetNumberOfInputPorts(2);
 }
 
 //----------------------------------------------------------------------------
@@ -32,8 +41,57 @@ int vtkCrossSectionImageFilter::RequestData(
     inImageInfo->Get(vtkDataObject::DATA_OBJECT()));
   vtkPointSet* inPoints = vtkPointSet::SafeDownCast(
     inPointsInfo->Get(vtkDataObject::DATA_OBJECT()));
-  vtkPolyData* output = vtkPolyData::SafeDownCast(
+  vtkDataSet* output = vtkDataSet::SafeDownCast(
     outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+  vtkSmartPointer<vtkProbeFilter> probe = vtkSmartPointer<vtkProbeFilter>::New();
+  probe->SetInputData(inPoints);
+  probe->SetSourceData(inImage);
+  probe->Update();
+
+  // Get the probe values from the output
+  vtkDataSet* probeOutput = probe->GetOutput();
+  if (!probeOutput)
+    {
+    vtkErrorMacro(<< "No output from probe filter");
+    return 0;
+    }
+
+  vtkPointSet* pointSet = vtkPointSet::SafeDownCast(probeOutput);
+  if (!pointSet)
+    {
+    vtkErrorMacro(<< "Probe filter did not return a vtkPointSet");
+    return 0;
+    }
+
+  // Threshold the image file to get rid of the NaNs
+  vtkSmartPointer<vtkThreshold> threshold = vtkSmartPointer<vtkThreshold>::New();
+  threshold->ThresholdBetween(0.0, 1.0);
+  threshold->AllScalarsOn();
+  threshold->SetInputData(inImage);
+
+  // Get the point data at the points and use these values as
+  // isosurface values for the heat flow image.
+  vtkIdType numberOfPoints = pointSet->GetNumberOfPoints();
+
+  vtkSmartPointer<vtkContourFilter> contourFilter =
+    vtkSmartPointer<vtkContourFilter>::New();
+  contourFilter->SetNumberOfContours(numberOfPoints);
+  contourFilter->ComputeNormalsOn();
+  contourFilter->GenerateTrianglesOn();
+  contourFilter->SetInputConnection(threshold->GetOutputPort());
+
+  vtkDataArray* da = pointSet->GetPointData()->GetScalars();
+  for (int i = 0; i < pointSet->GetNumberOfPoints(); ++i)
+    {
+    contourFilter->SetValue(i, da->GetTuple1(i));
+    }
+
+  // Create a contour filter and set the isovalues
+  contourFilter->Update();
+
+  // Copy output of contour filter to output
+  output->ShallowCopy(contourFilter->GetOutput());
 
   return 1;
 }
@@ -51,7 +109,7 @@ int vtkCrossSectionImageFilter::FillInputPortInformation(int port, vtkInformatio
     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPointSet");
     return 1;
     }
-    
+
   return 0;
 }
 
