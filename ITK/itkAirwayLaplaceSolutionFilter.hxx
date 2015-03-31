b84bbd16031c6240bfe58073be262c1775376854
Cory Quammen <cory.quammen@kitware.com>
2015-03-31 09:26:25 -0400

    Add connected component filter to ensure we have just one component
    This seems to solve problems where the sparse linear solver doesn't
    converge.

--- ITK/itkAirwayLaplaceSolutionFilter.hxx
+++ ITK/itkAirwayLaplaceSolutionFilter.hxx
@@ -3,7 +3,10 @@
 
 #include "itkAirwayLaplaceSolutionFilter.h"
 #include "itkAirwayLaplaceBoundaryImageFilter.h"
+#include <itkBinaryThresholdImageFilter.h>
+#include <itkConnectedComponentImageFilter.h>
 #include "itkLaplaceEquationSolverImageFilter.h"
+#include <itkRelabelComponentImageFilter.h>
 #include "itkImageRegionIteratorWithIndex.h"
 #include <itkInvertIntensityImageFilter.h>
 #include <itkBinaryContourImageFilter.h>
@@ -11,6 +14,9 @@
 #include "itkVector.h"
 #include <stdio.h>
 #include <math.h>
+
+// TEMPORARY
+#include <itkImageFileWriter.h>
 
 namespace itk
 {
@@ -36,16 +42,45 @@ AirwayLaplaceSolutionFilter< TInputImage, TOutputImage >
   this->AllocateOutputs();
   output->FillBuffer( 0 );
 
+  // Extract largest connected component from binary image. If we don't
+  // do this, the Laplace solution will be undefined (maybe).
+  typedef itk::ConnectedComponentImageFilter< TInputImage, TInputImage, TInputImage > ConnectedFilterType;
+  typename ConnectedFilterType::Pointer connectedFilter = ConnectedFilterType::New();
+  connectedFilter->SetInput( input );
+
+  typedef itk::RelabelComponentImageFilter< TInputImage, TInputImage > RelabelFilterType;
+  typename RelabelFilterType::Pointer relabelFilter = RelabelFilterType::New();
+  relabelFilter->SetInput( connectedFilter->GetOutput() );
+
+  typedef itk::BinaryThresholdImageFilter< TInputImage, TInputImage > ThresholdType;
+  typename ThresholdType::Pointer thresholdFilter = ThresholdType::New();
+  thresholdFilter->SetInsideValue( 1 );
+  thresholdFilter->SetOutsideValue( 0 );
+  thresholdFilter->SetLowerThreshold( 1 );
+  thresholdFilter->SetUpperThreshold( 1 );
+  thresholdFilter->SetInput( relabelFilter->GetOutput() );
+
   // Identify Boundary
   typedef itk::AirwayLaplaceBoundaryImageFilter< TInputImage > BoundaryFilter;
   typename BoundaryFilter::Pointer boundaryFilter = BoundaryFilter::New();
-  boundaryFilter->SetInput( input );
+  boundaryFilter->SetInput( thresholdFilter->GetOutput() );
   boundaryFilter->SetNasalPoint(    m_NosePoint   );
   boundaryFilter->SetNasalVector(   m_NoseVector  );
   boundaryFilter->SetTracheaPoint(  m_TrachPoint  );
   boundaryFilter->SetTracheaVector( m_TrachVector );
   boundaryFilter->Update();
 
+  // TEMPORARY - write out boundary image
+  typedef itk::ImageFileWriter< TInputImage > WriterType;
+  typename WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName("BOUNDARY_CONDITION_IMAGE.mha");
+  writer->SetInput( boundaryFilter->GetOutput() );
+  writer->Update();
+
+  typename WriterType::Pointer ccWriter = WriterType::New();
+  ccWriter->SetFileName("CONNECTED_COMPONENTS_IMAGE.mha");
+  ccWriter->SetInput( thresholdFilter->GetOutput() );
+  ccWriter->Update();
 
   // // Solve Laplace Equation
   typedef itk::LaplaceEquationSolverImageFilter< TInputImage, TOutputImage > LaplaceFilterType;
