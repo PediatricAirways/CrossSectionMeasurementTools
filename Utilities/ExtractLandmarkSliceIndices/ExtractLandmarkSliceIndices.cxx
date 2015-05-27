c5683295513a11352b597f37b6ceebadd3e0efda
Cory Quammen <cory.quammen@kitware.com>
2015-05-27 16:47:26 -0400

    Do not depend on order of landmark values in arrays

--- Utilities/ExtractLandmarkSliceIndices/ExtractLandmarkSliceIndices.cxx
+++ Utilities/ExtractLandmarkSliceIndices/ExtractLandmarkSliceIndices.cxx
@@ -1,8 +1,10 @@
-#include <vtkXMLPolyDataReader.h>
-#include <vtkSmartPointer.h>
 #include <vtkDoubleArray.h>
 #include <vtkFieldData.h>
 #include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkStringArray.h>
+#include <vtkXMLPolyDataReader.h>
+
 #include <iostream>
 #include <fstream>
 
@@ -35,10 +37,49 @@ int main ( int argc, char *argv[] )
 
   vtkSmartPointer<vtkDoubleArray> area = vtkSmartPointer<vtkDoubleArray>::New();
 
+  // Find TVC
+  vtkFieldData* fd = polydata->GetFieldData();
+  vtkStringArray* names = vtkStringArray::SafeDownCast(fd->GetAbstractArray("query point name"));
+  if (!names)
+    {
+    std::cerr << "Could not find 'query point name' string array in input\n";
+    return EXIT_FAILURE;
+    }
 
-  myfile << polydata->GetFieldData()->GetArray("contour ID")->GetTuple(0)[0]<<std::endl; //TVC
-  myfile << polydata->GetFieldData()->GetArray("contour ID")->GetTuple(2)[0]<<std::endl; //Inferrior Subglottis
-  myfile << polydata->GetFieldData()->GetArray("contour ID")->GetTuple(3)[0]<<std::endl; //Trachea Carina
+  vtkIdType tvcID                = names->LookupValue( "TVC" );
+  vtkIdType inferiorSubglottisID = names->LookupValue( "InferiorSubglottis" );
+  vtkIdType tracheaCarinaID      = names->LookupValue( "TracheaCarina" );
+
+  // TVC
+  if ( tvcID >= 0 )
+    {
+    myfile << fd->GetArray("contour ID")->GetTuple(tvcID)[0] << std::endl;
+    }
+  else
+    {
+    myfile << -1 << std::endl;
+    }
+
+  // Inferior subglottis
+  if ( inferiorSubglottisID >= 0 )
+    {
+    myfile << polydata->GetFieldData()->GetArray("contour ID")->GetTuple(inferiorSubglottisID)[0] << std::endl;
+    }
+  else
+    {
+    myfile << -1 << std::endl;
+    }
+
+  // Trachea carina
+  if ( tracheaCarinaID )
+    {
+    myfile << polydata->GetFieldData()->GetArray("contour ID")->GetTuple(tracheaCarinaID)[0] << std::endl;
+    }
+  else
+    {
+    myfile << -1 << std::endl;
+    }
+
   myfile.close();
  
   return EXIT_SUCCESS;
